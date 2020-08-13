54de6d64d087c67679186f10e2d218482b778da2
Harrison Higgins <harryrhiggins@gmail.com>
2020-08-13 12:04:27 -0600

    Finished 'Start a New Game'

--- connect-four.js
+++ connect-four.js
@@ -1,16 +1,44 @@
+import { Game } from './game.js';
 
-// window.addEventListener("DOMContentLoaded", e => {
-
-// });
-
-const game = undefined;
+let game = undefined;
 
 document.getElementById("form-holder").addEventListener("keyup", e => {
     const player1Name = document.getElementById("player-1-name");
     const player2Name = document.getElementById("player-2-name");
-    
-    if(player1Name.value !== '' && player2Name.value !== '') {
-        const newGameBtn = document.getElementById("new-game");
-        newGameBtn.disabled = false;
+
+    if (player1Name.value !== '' && player2Name.value !== '') {
+        disableNewGame(false);
     }
 });
+
+document
+    .getElementById('new-game')
+    .addEventListener('click', event => {
+        const player1Name = document.getElementById("player-1-name");
+        const player2Name = document.getElementById("player-2-name");
+
+        game = new Game(player1Name.value, player2Name.value)
+
+        player1Name.value = '';
+        player2Name.value = '';
+        disableNewGame(true);
+
+        updateUI();
+    });
+
+function disableNewGame(bool) {
+    const newGameBtn = document.getElementById("new-game");
+    newGameBtn.disabled = bool;
+}
+
+function updateUI() {
+    if (game === undefined) {
+        document.getElementById('board-holder')
+            .classList.add('is-invisible');
+    } else {
+        document.getElementById('board-holder')
+            .classList.remove('is-invisible');
+        document.getElementById('game-name')
+            .innerHTML = game.getName();
+    }
+}
